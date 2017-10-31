f348dec495ecb941b2c4efa1efed554316bb949b
umayr96 <umayrandzubair@hotmail.com>
2017-10-31 18:12:18 -0400

    creating the flow of the app

--- app/src/main/java/com/capstone/ridewithus/MainActivity.java
+++ app/src/main/java/com/capstone/ridewithus/MainActivity.java
@@ -1,13 +1,28 @@
 package com.capstone.ridewithus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById (R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
