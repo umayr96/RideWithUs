550e3287dfbc5f9c1d7a8b4ceefd06b3ed1eb53e
umayr96 <umayrandzubair@hotmail.com>
2017-12-31 12:23:52 -0500

    made the signout page work

--- app/src/main/java/com/capstone/ridewithus/SignoutActivity.java
+++ app/src/main/java/com/capstone/ridewithus/SignoutActivity.java
@@ -1,13 +1,22 @@
 package com.capstone.ridewithus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_signout);
+        FirebaseAuth.getInstance().signOut(); // signing out the currnet user
+        Intent intent = new Intent(SignoutActivity.this, MainActivity.class);// going back to the main activity
+        startActivity(intent);
+        finish();
+        return;
     }
 }
